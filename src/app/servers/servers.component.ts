b83f6defcacc95b2295b96a58933503ed4776273
Andri Gunawan <andri_gunawan@msn.com>
2019-08-03 19:24:09 -0700

    Using *ngIF

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  isServerCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.isServerCreated = true;
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
   }
 
